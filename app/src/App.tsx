b54c5c6fa03777e885432f18c30bb170537254b5
AldoOmarAndres <aldoandres045@gmail.com>
2024-11-22 16:02:01 -0300

    Listado y seleccion de capas funcionando

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -17,75 +17,79 @@ type CapaParams = {
   isVisible: boolean;
 };
 
+const capasArray: CapaParams[] = [];
+CAPAS.map((c) =>
+  capasArray.push({
+    id: c,
+    nombre: c
+      .toLocaleLowerCase()
+      .split("_")
+      .map((nombre) => nombre.charAt(0).toUpperCase() + nombre.slice(1))
+      .join(" "),
+    isVisible: false,
+  })
+);
+
+const osm = new TileLayer({
+  source: new OSM(),
+});
+
+const URL = import.meta.env.VITE_QGIS_SERVER_URL;
 function App() {
   const mapRef = useRef<HTMLDivElement | null>(null);
   const [map, setMap] = useState<Map | null>(null);
   const [capasVisibles, setCapasVisibles] = useState<string[]>([]);
-
-  //Se puede hacer menos verboso creo.., tengo sueño :(
-  const capasArray: CapaParams[] = [];
-  CAPAS.map((c) =>
-    capasArray.push({
-      id: c,
-      nombre: c
-        .toLocaleLowerCase()
-        .split("_")
-        .map((nombre) => nombre.charAt(0).toUpperCase() + nombre.slice(1))
-        .join(" "),
-      isVisible: false,
-    })
-  );
-
-  //const URL = process.env.VITE_QGIS_SERVER_URL;
+  const [crc, setCrc] = useState<string>("EPSG:22175")
 
   useEffect(() => {
-    //if (!mapRef.current) return;
+    if (!mapRef.current) return;
 
     // Crear el mapa
-    /* const mapa = new Map({
+    const mapa = new Map({
       target: mapRef.current,
-        layers: [
-          new TileLayer({
-            source: new TileWMS({
-              url: "" + URL,
-              params: {
-                SERVICE: "WMS",
-                VERSION: "1.3.0",
-                REQUEST: "GetMap",
-                LAYERS: capasVisibles,
-                FORMAT: "image/png",
-                CRS: "EPSG:22175"
-              },
-              serverType: "qgis",
-            }),
+      layers: [osm,
+        new TileLayer({
+          source: new TileWMS({
+            url: "" + URL,
+            params: {
+              SERVICE: "WMS",
+              VERSION: "1.3.0",
+              REQUEST: "GetMap",
+              LAYERS: capasVisibles,
+              FORMAT: "image/png",
+              CRS: crc,
+            },
+            serverType: "qgis",
           }),
-        ],
-        view: new View({
-          center: fromLonLat([-64.0, -34.0]), // Coordenadas iniciales para Arg
-          zoom: 4,
         }),
-      });
+        
+      ],
+      view: new View({
+        center: fromLonLat([-64.0, -34.0]), // Coordenadas iniciales para Arg
+        zoom: 5,
+      }),
+    });
 
-      setMap(mapa); */
+    setMap(mapa);
     console.log("capas visibles:", capasVisibles);
 
     // Limpiar el mapa cuando el componente se desmonte
-    //return () => map?.setTarget(undefined);
+    return () => map?.setTarget(undefined);
   }, [capasVisibles]);
 
   // Manejar cambios de visibilidad de capas
   const toggleCapa = (capaSelected: string) => {
-    const capa = capasArray.find((c) => c.nombre === capaSelected);
-    
+    const capa = capasArray.find((c) => c.id === capaSelected);
+
     if (capa) {
       const newCapa = capa;
       newCapa.isVisible = !capa.isVisible;
       capasArray.splice(capasArray.indexOf(capa), 1, newCapa);
-      console.log("Nuevo array", capasArray)
+      console.log("Nuevo array", capasArray);
 
       console.log("Lo incluye?", capasVisibles.includes(capa.id));
 
-      //Muy verboso esto
+      
       if (!capasVisibles.includes(newCapa.id)) {
         newCapa.isVisible
           ? setCapasVisibles((prev) => [...prev, newCapa.id])
@@ -99,11 +103,12 @@ function App() {
     console.log(capasVisibles);
   };
 
-  console.log(capasArray);
-
   return (
     <div style={{ display: "flex" }}>
-      {/* Menú de capas */}
+      {/* Menú de capas 
+      // TODO: mejorar visual de listado
+          //Se listan con nombres incompletos, los mismos que en array "CAPAS"
+      */}
       <div style={{ width: "280px", padding: "10px", background: "gray" }}>
         <h3>Capas</h3>
         <ul style={{ listStyle: "none", padding: 0 }}>
@@ -113,7 +118,7 @@ function App() {
                 <input
                   type="checkbox"
                   checked={capa.isVisible}
-                  onChange={() => toggleCapa(capa.nombre)}
+                  onChange={() => toggleCapa(capa.id)}
                 />
                 {capa.nombre}
               </label>
@@ -125,7 +130,7 @@ function App() {
       {/* Contenedor del mapa */}
       <div
         ref={mapRef}
-        style={{ flex: 1, width: "100%", height: "100%" }}
+        style={{ flex: 1, width: "100%", height: "100hv" }}
       ></div>
     </div>
   );
